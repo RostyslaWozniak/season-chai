a99ba947f581c0b757b4853f20b01f70c140a33c
Rostyslav Vozniak <rostik19wozniak@icloud.com>
2024-10-01 19:07:39 +0200

    fix cart sheet triggering open

--- src/components/cart/CartSheet.tsx
+++ src/components/cart/CartSheet.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
@@ -22,26 +22,18 @@ import { cn } from "@/lib/utils";
 import { RemoveCartItem } from "./RemoveCartItem";
 import { PriceView } from "../products/PriceView";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { CartSheetTrigger } from "./CartSheetTrigger";
 
 export function CartSheet() {
   const { cartItems, totalItems, totalPrice } = useCart();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   const { user } = useSession();
   const router = useRouter();
-
-  const searchParams = useSearchParams();
-  const isOpen = searchParams.get("cart-open") === "true";
-  const q = searchParams.get("q");
-  const href = isOpen
-    ? q
-      ? `?q=${q}&cart-open=false`
-      : "?cart-open=false"
-    : q
-      ? `?q=${q}&cart-open=true`
-      : `?cart-open=true`;
 
   if (!user)
     return (
@@ -61,9 +53,8 @@ export function CartSheet() {
     );
 
   return (
-    <Sheet open={isOpen}>
-      <CartSheetTrigger />
-
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <CartSheetTrigger isOpen={isOpen} setIsOpen={setIsOpen} />
       <SheetContent
         side="right"
         className="z-50 flex w-[min(100%,500px)] flex-col gap-0 p-0 pb-20 sm:min-w-[500px] md:pb-0"
@@ -73,16 +64,15 @@ export function CartSheet() {
           <SheetDescription>
             You have {cartItems.length} items in your cart.
           </SheetDescription>
-          <Link
-            href={href}
-            className={cn(
-              buttonVariants({ variant: "outline", size: "icon" }),
-              "absolute right-4 top-4 rounded-sm border opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary",
-            )}
+          <Button
+            onClick={() => setIsOpen(false)}
+            className="absolute right-4 top-4 rounded-sm border opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
+            size="icon"
+            variant="outline"
           >
             <Cross2Icon className="h-7 w-7 md:h-6 md:w-6" />
             <span className="sr-only">Close</span>
-          </Link>
+          </Button>
         </SheetHeader>
         {cartItems.length === 0 && <EmptyCart />}
 
--- src/components/cart/CartSheetTrigger.tsx
+++ src/components/cart/CartSheetTrigger.tsx
@@ -4,23 +4,19 @@ import { useCart } from "@/context/CartContext";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { buttonVariants } from "../ui/button";
+import { Button } from "../ui/button";
 
-export const CartSheetTrigger = () => {
+type CartSheetTriggerProps = {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const CartSheetTrigger = ({
+  isOpen,
+  setIsOpen,
+}: CartSheetTriggerProps) => {
   const { cartItems, totalItems } = useCart();
 
-  const searchParams = useSearchParams();
-  const isOpen = searchParams.get("cart-open") === "true";
-  const q = searchParams.get("q");
-  const href = isOpen
-    ? q
-      ? `?q=${q}&cart-open=false`
-      : "?cart-open=false"
-    : q
-      ? `?q=${q}&cart-open=true`
-      : `?cart-open=true`;
   return (
     <motion.div
       className={cn("flex flex-col items-center", {
@@ -30,12 +26,11 @@ export const CartSheetTrigger = () => {
       initial={cartItems.length > 0}
       animate={{ rotate: [0, -5, 5, -5, 5, 0] }}
     >
-      <Link
-        href={href}
-        className={cn(
-          buttonVariants({ variant: "outline", size: "icon" }),
-          "pointer-events-auto relative flex flex-col items-center rounded-full p-2 text-gray-600 transition-colors duration-200",
-        )}
+      <Button
+        variant="outline"
+        size="icon"
+        className="pointer-events-auto relative flex flex-col items-center rounded-full p-2 text-gray-600 transition-colors duration-200"
+        onClick={() => setIsOpen(!isOpen)}
       >
         <span
           className={cn(
@@ -54,12 +49,12 @@ export const CartSheetTrigger = () => {
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
             exit={{ scale: 0 }}
-            className="absolute -bottom-2 -right-2 grid aspect-square h-5 place-items-center rounded-full bg-primary text-primary-foreground md:-bottom-2"
+            className="absolute -bottom-2 -right-2 aspect-square h-5 rounded-full bg-primary text-primary-foreground md:-bottom-2"
           >
             {cartItems.length}
           </motion.span>
         )}
-      </Link>
+      </Button>
       <span className="mt-1 text-center text-xs font-medium md:hidden">
         Cart
       </span>
