3f953bbfbac80c59a06b1d6a50202fd8a1749ae8
Rostyslav Vozniak <rostik19wozniak@icloud.com>
2024-10-02 08:21:16 +0200

    text dynamic og images

--- src/app/(main)/products/[id]/opengraph-image.tsx
+++ src/app/(main)/products/[id]/opengraph-image.tsx
@@ -2,8 +2,6 @@
 import { api } from "@/trpc/server";
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
-
 export const size = { width: 1200, height: 630 };
 
 export const contentType = "image/png";
